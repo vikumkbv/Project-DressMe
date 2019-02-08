ac8e785740f7b9b53b1c935d48af0034b7f097ba
vikumkbv <vikum.2017116@iit.ac.lk>
2019-02-08 23:06:08 +0530

    remove background black + get colors

--- DressMe-Mobile-App/screens/HomeScreen.js
+++ DressMe-Mobile-App/screens/HomeScreen.js
@@ -32,10 +32,10 @@ export default class HomeScreen extends React.Component {
             />
           </View>
 
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
+          /* <View style={styles.getStartedContainer}>
+            {this._maybeRenderDevelopmentModeWarning()} */
 
-            <Text style={styles.getStartedText}>Get started by opening</Text>
+            <Text style={styles.getStartedText}>Get started by njljnk  opening</Text>
 
             <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
               <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
@@ -48,7 +48,7 @@ export default class HomeScreen extends React.Component {
 
           <View style={styles.helpContainer}>
             <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
+              <Text style={styles.helpLinkText}>Help, it didn’tfdsf automatically reload!</Text>
             </TouchableOpacity>
           </View>
         </ScrollView>
--- Suggestions/skin.py
+++ Suggestions/skin.py
@@ -31,3 +31,82 @@ def extractSkin(image):
 
     # Return the image
     return cv2.cvtColor(skin, cv2.COLOR_HSV2BGR)
+
+def removeBlack(estimator_labels, estimator_cluster):
+
+    # Check for black
+    hasBlack = False
+
+    # Get the total number of occurance for each color
+    occurance_counter = Counter(estimator_labels)
+
+    # Quick lambda function to compare to lists
+    def compare(x, y): return Counter(x) == Counter(y)
+
+    # Loop through the most common occuring color
+    for x in occurance_counter.most_common(len(estimator_cluster)):
+
+        # Quick List comprehension to convert each of RBG Numbers to int
+        color = [int(i) for i in estimator_cluster[x[0]].tolist()]
+
+        # Check if the color is [0,0,0] that if it is black
+        if compare(color, [0, 0, 0]) == True:
+            # delete the occurance
+            del occurance_counter[x[0]]
+            # remove the cluster
+            hasBlack = True
+            estimator_cluster = np.delete(estimator_cluster, x[0], 0)
+            break
+
+    return (occurance_counter, estimator_cluster, hasBlack)
+
+
+def getColorInformation(estimator_labels, estimator_cluster, hasThresholding=False):
+
+    # Variable to keep count of the occurance of each color predicted
+    occurance_counter = None
+
+    # Output list variable to return
+    colorInformation = []
+
+    # Check for Black
+    hasBlack = False
+
+    # If a mask has be applied, remove th black
+    if hasThresholding == True:
+
+        (occurance, cluster, black) = removeBlack(
+            estimator_labels, estimator_cluster)
+        occurance_counter = occurance
+        estimator_cluster = cluster
+        hasBlack = black
+
+    else:
+        occurance_counter = Counter(estimator_labels)
+
+    # Get the total sum of all the predicted occurances
+    totalOccurance = sum(occurance_counter.values())
+
+    # Loop through all the predicted colors
+    for x in occurance_counter.most_common(len(estimator_cluster)):
+
+        index = (int(x[0]))
+
+        # Quick fix for index out of bound when there is no threshold
+        index = (index-1) if ((hasThresholding & hasBlack)
+                              & (int(index) != 0)) else index
+
+        # Get the color number into a list
+        color = estimator_cluster[index].tolist()
+
+        # Get the percentage of each color
+        color_percentage = (x[1]/totalOccurance)
+
+        # make the dictionay of the information
+        #colorInfo = {"cluster_index": index, "color": color,"color_percentage": color_percentage}
+        colorInfo = color
+        
+        # Add the dictionary to the list
+        colorInformation.append(colorInfo)
+
+    return colorInformation
