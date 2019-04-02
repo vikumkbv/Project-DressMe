e05d51aff2418e154d6bd05f6354d4732e988fc4
Jonathan Chanuka Gurusinghe <36181375+JonathanTechsl1998@users.noreply.github.com>
2019-04-02 15:28:21 +0530

    configuration for dialogflow

---
+++ DressMe_App/config.js
@@ -0,0 +1,5 @@
+var config = (
+    "dialogflow-iohfhk@dressme-6a542.iam.gserviceaccount.com",
+    "884354d738b286f66c87aeda5965ddd2ff40a7a7",
+    Dialogflow_V2.LANG_ENGLISH_US,
+    "dressme-6a542");
